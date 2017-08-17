7d8e20528a29ead1e2562e16eb23a704ea07dc75
createname <zengyongeryu@163.com>
2017-08-17 10:17:06 +0800

    Initial Commit

---
+++ ZYJDDetail/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYJDDetail
+//
+//  Created by liqiaona on 2017/8/17.
+//  Copyright © 2017年 ZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZYJDDetail/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZYJDDetail
+//
+//  Created by liqiaona on 2017/8/17.
+//  Copyright © 2017年 ZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
